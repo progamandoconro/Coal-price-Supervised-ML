70b2f74cd6bb3f88f732d243d363c065194b897e
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-14 14:29:25 +0100

    Update R_script.R

--- R_script.R
+++ R_script.R
@@ -1,1 +1,14 @@
-df <- read.csv('data_supervised.csv')
+# Coal future price prediction.	
+# 2019. Rodrigo Diaz-Lupanow (programandoconro). Code for private client.  
+
+
+	df <- read.csv('data_supervised.csv')
+
+	precio <- df$DLI_PRECIO_A_PAGAR.x ; valor <- df$DLI_VALOR_TOTAL.x ; peso <- df$DLI_PESO_A_PAGAR.x
+
+	mes <- df2$mes.n + 1 ; mes <- ifelse(mes==13,1, mes)
+
+	dat_cruzada <- data.frame(mes,peso,valor,precio)
+
+  
+
