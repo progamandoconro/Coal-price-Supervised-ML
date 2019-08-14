537506c18b84a921e050f091e2c88612d8356d31
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-14 15:04:51 +0100

    Update R_script.R

--- R_script.R
+++ R_script.R
@@ -6,7 +6,7 @@
 
 	precio <- df$DLI_PRECIO_A_PAGAR.x ; valor <- df$DLI_VALOR_TOTAL.x ; peso <- df$DLI_PESO_A_PAGAR.x
 
-	mes <- df$mes.n - 1 ; mes <- ifelse(mes == 0, 12, mes)
+	mes <- df$Mes.n - 1 ; mes <- ifelse(mes == 0, 12, mes)
 
 	dat_cruzada <- data.frame(mes,peso,valor,precio)
 
