ec655090c3d56458c528302596ae9322126972a1
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-15 15:45:53 +0100

    Update R_script.R

--- R_script.R
+++ R_script.R
@@ -3,12 +3,15 @@
 
 
 	df <- read.csv('data_supervised.csv')
+	df$anio_mes <- paste(df[,1],df[,2])
+	
+	library(ROSE)
+	
+	df <- ROSE(df$anio_mes~., data=df)
 
 	valor <- df$DLI_VALOR_TOTAL.x ; peso <- df$DLI_PESO_A_PAGAR.x
 
 	mes <- df$Mes.n + 1 ; mes <- ifelse(mes == 12, 1, mes)
 
-	dat_cruzada <- data.frame(mes,peso,valor)
 
-  
 
