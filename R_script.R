3b3d67c839cc571c08ec68d7a3c588358ad9990c
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-15 16:06:02 +0100

    Update R_script.R

--- R_script.R
+++ R_script.R
@@ -1,15 +1,16 @@
 # Coal future price prediction.	
 # 2019. Rodrigo Diaz-Lupanow (programandoconro). Code for private client.  
 
+	library (dplyr)
 
 	df <- read.csv('data_supervised.csv')
 	df$anio_mes <- paste(df[,1],df[,2])
 
-	valor <- df$DLI_VALOR_TOTAL.x ; peso <- df$DLI_PESO_A_PAGAR.x
+	df$valor <- df$DLI_VALOR_TOTAL.x ; df$peso <- df$DLI_PESO_A_PAGAR.x
 
 	mes_dc <- df$Mes.n + 1 ; mes_dc <- ifelse(mes_dc == 12, 1, mes_dc)
 	
-	df_2 <- data.frame (valor, peso, Mes.n=mes_dc)
+	df_2 <- data.frame (df$valor, df$peso, Mes.n=mes_dc)
 
 	bind_rows <- (df,df_2)
 
