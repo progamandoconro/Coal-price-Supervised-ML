f1b770336aba528ad1f98ac5c4203bab0a6a9c6c
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-15 16:01:37 +0100

    Update R_script.R

--- R_script.R
+++ R_script.R
@@ -4,14 +4,14 @@
 
 	df <- read.csv('data_supervised.csv')
 	df$anio_mes <- paste(df[,1],df[,2])
-	
-	library(ROSE)
-	
-	df <- ROSE(df$anio_mes~., data=df)
 
 	valor <- df$DLI_VALOR_TOTAL.x ; peso <- df$DLI_PESO_A_PAGAR.x
 
-	mes <- df$Mes.n + 1 ; mes <- ifelse(mes == 12, 1, mes)
+	mes_dc <- df$Mes.n + 1 ; mes_dc <- ifelse(mes_dc == 12, 1, mes_dc)
+	
+	df_2 <- data.frame (valor, peso, Mes.n=mes_dc)
 
+	bind_rows <- (df,df_2)
 
+	
 
