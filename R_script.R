ebe6c016ab96e77cb4b645aaaa2d08b871e31684
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-21 16:13:16 +0100

    Update R_script.R

--- R_script.R
+++ R_script.R
@@ -1,30 +1,57 @@
-# Coal future price prediction.	
+# Precio Futuro del .	
 # 2019. Rodrigo Diaz-Lupanow (programandoconro). Code for private client.  
-
-	library (dplyr)
-
-	input <- read.csv('data_supervised.csv')
-	
-
-	############################################################################################################
-################################################## Litecoin ##################################################
-############################################################################################################
 
 library(dplyr) # Para facilitar la mineria de datos
 library(zoo) # Tratamiento para los datos faltantes
 library(lubridate) # Tratamiento para las fechas en la data
 
-######################## Normalizar la data entre valores desde 0 hasta 1 ###########################################
- 
-
-                normalize <- function(x) { 
+ normalize <- function(x) { 
   return((x - min(x)) / (max(x) - min(x)))
 }
-#input<- lapply(input, normalize)%>%
- # as.data.frame()
 
-##input_60 <- input[(nrow(input)-60):nrow(input),]  
-#input <- input[1:(nrow(input)-61),] 
+	
+	
+
+	############################################################################################################
+################################################## Valor a pagar ##################################################
+############################################################################################################
+
+######################## Data ###########################################	
+######################## Normalizar la data entre valores desde 0 hasta 1 ###########################################
+
+input <- read.csv('data_supervised.csv')[,-18:-19]%>%
+               lapply( normalize)%>%
+		as.data.frame() 
+            
+
+################################# Guardemos los 600 datos como test final ###################################
+
+input_600 <- input[(nrow(input)-600):nrow(input),]  
+input <- input[1:(nrow(input)-601),] 
+
+
+################################ Mezclemos la data y guardemos una parte para validar #######################
+
+index = sample (1:nrow(input),nrow(input))
+
+train = input[1:(NROW(index)* 0.7),]
+valida = input[-1:-(NROW(index)* 0.7),]
+
+# Algoritmo 1
+
+library(randomForest)
+
+set.seed(7)   
+rf <-  randomForest(train$DLI_VALOR_TOTAL.x ~.,data = train[,-train$DLI_VALOR_TOTAL.x])
+
+res_val <- predict (rf,valida)
+
+cor (res_val, valida$DLI_VALOR_TOTAL.x)
+
+#################################### Veamos la data test #######################################
+
+################################### Escojamos un modelo utilizando Akaike ################################## 
+
 
 #library(MASS)  
 #m = stepAIC(glm(d_train$OUTPUT~.,data = d_train[,-d_train$OUTPUT]))
@@ -34,21 +61,19 @@ library(lubridate) # Tratamiento para las fechas en la data
 
 ############ Ejecutar el algoritmo Random Forest #############################################################
 
-library(randomForest)
-
 set.seed(7)   
-#rf <-  randomForest(formula,data = d_train[,-d_train$OUTPUT])
+#rf <-  randomForest(m$formula,data = d_train[,-d_train$OUTPUT])
               
 #p_60 <- predict(rf, input_60[,colnames(input[,-input$OUTPUT]) ])
 ##################### Graficar los resultados #################################################################
                 
-jpeg('plot_carbon.jpg')
+#jpeg('plot_carbon.jpg')
                 
-plot(1,1,xlab = 'Días desde creación', ylab = 'Precio escalado ($ COL)')
+#plot(1,1,xlab = 'Días desde creación', ylab = 'Precio escalado ($ COL)')
 #points(p_60,col=2)  
 #lines(p_60,col=2)  
 #lines(input_60$OUTPUT)
                 
-dev.off() 
+#dev.off() 
 	
 
