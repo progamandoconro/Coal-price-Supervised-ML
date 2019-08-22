f44da5d8cb76f802a787dd20769173e15cc187a3
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-22 20:47:31 +0100

    Rename R_script.R to script.R

---
+++ script.R
@@ -0,0 +1,35 @@
+###################### Data Cruzada 1 ###############################
+
+	library(dplyr) # Código con arquitectura de tuberías (%>%) 
+	library(zoo) # Homogeneizar la cantidad de datos por mes
+       	library(lubridate)
+	library(dplyr)
+	library(stringr)
+
+# Funciones necesarias:
+
+	Normalizar <- function(x) { 
+  	return((x - min(x)) / (max(x) - min(x)))
+	}
+
+	RMSE = function(esperados, observados){
+  	sqrt(mean((esperados - observados)^2))
+	}
+
+# Data base
+
+	df = read.csv("FD-04_minero_21062019.csv")
+
+
+	fecha <- parse_date_time(dat_1$ENT_FECHA_ENTRADA,
+	orders = "%Y-%m-%d %H:%M:%S")
+
+	mes <- month(as.POSIXlt(fecha, format="%Y-%m-%d %H:%M:%S"))
+
+	anio <- year(as.POSIXlt(fecha, format="%Y-%m-%d %H:%M:%S"))
+
+	mes_anio <-paste(anio,mes)
+
+	input = df [, c(-3,-4,-6)]
+
+
