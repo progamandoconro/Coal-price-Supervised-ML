1dcc7b4971adbb3e1d1c96f4cd7493510cb400e6
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-09-11 14:03:19 +0100

    Update supervised.R

--- supervised.R
+++ supervised.R
@@ -3,19 +3,13 @@ library(zoo)
 library(randomForest)
 library(groupdata2)
 library(e1071)
-
-RMSE = function(esperados, observados){
-sqrt(mean((esperados - observados)^2))
-} 
-
+library(caret)
 
 normalize <- function(x) { 
   return((x - min(x)) / (max(x) - min(x)))
 }
 
-
 df=read.csv('carbon_3.csv')
-
 
 df <- select (df,-starts_with("date"))%>%
   select(-starts_with("date."))%>%
@@ -58,3 +52,24 @@ df_cruz$output<- output
 
 df_cruz<-df_cruz[1:(nrow(df_cruz)-n),]
 
+#test= df_cruz[(nrow(df_cruz)-220):(nrow(df_cruz)),]
+
+###################################################
+
+#df_cruz <- df_cruz[1:(nrow(df_cruz)-220),]
+set.seed(777)
+
+index <- sample(1:nrow(df_cruz),nrow(df_cruz))
+
+train <- df_cruz[1:floor(nrow(df_cruz)*0.7),]
+
+val <- df_cruz[(floor(nrow(df_cruz)*0.7)+1):nrow(df_cruz),]
+
+rF <- randomForest (as.factor(train$output)~., data=train, scale=T)
+svm <- svm (as.factor(train$output)~., data=train, scale=T,type="C-Classification")
+            
+p <- predict(svm, as.factor(val))
+p2 <- predict(rF, as.factor(val))
+
+confusionMatrix(p,as.factor(val[,1]))
+confusionMatrix(p2,as.factor(val[,1]))
