d17b688da326c45d305d2a3f07ed3d4acecd37f8
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-09-13 20:43:00 +0100

    Update app.R

--- app.R
+++ app.R
@@ -34,7 +34,7 @@ ui <- dashboardPage(
 dashboardHeader(title="Predicción Carbón"),
 dashboardSidebar(h5("Parámetros de afinación del Algoritmo Random Forest"),
 numericInput("p1","ntree",534),
-numericInput("p2","mtry",200),
+numericInput("p2","mtry",sqrt(200)),
 h5("*El valor de mtry es llevado a su raíz cuadrada"),
 numericInput("p3","seed",777),
 h5("Predicciones"),
@@ -42,14 +42,61 @@ sliderInput("p4","Ajuste de probabilidad de clases",min=0.1,max=0.9,value=0.5),
 sliderInput("p5","Número de meses a futuro",min=1,max=12,value=c(1,1))
 
 ),
-  dashboardBody(tabItem('item',tabsetPanel(tabPanel('Validación'),
+  dashboardBody(tabItem('item',tabsetPanel(tabPanel('Validación',h5('hello')),
 tabPanel('Evaluación'),tabPanel('Predicciones')
 
 ))))
 
-server <- function(input, output) { }
+server <- function(input, output) {
+
+
+m=1
+
+n=28*m
+
+input<- df
+
+output<-df$DLI_PESO_A_PAGAR
+
+df_cruz <- data.frame(output,input)
+
+output<- vector()
+ 
+for (i in 1:NROW(df_cruz[,1])) {
+ 
+output[i]<-ifelse( df_cruz[i,1]<df_cruz[i+n,1],1,0 )
+
+ }
+
+df_cruz$output<- output
+
+df_cruz<-df_cruz[1:(nrow(df_cruz)-n),]
+
+test= df_cruz[(nrow(df_cruz)-220):(nrow(df_cruz)),]
+
+df_cruz <- df_cruz[1:(nrow(df_cruz)-220),]
+set.seed(input$p3)
+
+index <- sample(1:nrow(df_cruz),nrow(df_cruz))
+
+train <- df_cruz[1:floor(nrow(df_cruz)*0.7),]
+
+val <- df_cruz[(floor(nrow(df_cruz)*0.7)+1):nrow(df_cruz),]
+
+rF <- randomForest (as.factor(train$output)~.,ntree=533, mtry=sqrt(200) ,data=train[,-1], scale=T, importance=T,replace=T)
+
+p2 <- predict(rF, val[,-1])
+l <- confusionMatrix(p2,as.factor(val[,1]))
+
+
+
+}
+
+ 
 
 shinyApp(ui, server)
+
+
 
 
 
